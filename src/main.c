1d6d66392e7ad5f1302d5a9ace400cecd97dbec8
PatrikBakyta <patrikbakyta@gmail.com>
2016-11-27 13:37:15 +0100

    Funkcia initLED

--- src/main.c
+++ src/main.c
@@ -85,6 +85,24 @@ void initADC(){
 
 }
 
+void initLED(){
+
+	// spustenie periferie
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+
+	// nastavanie GPIO pinu 5 (LED2)
+	GPIO_InitTypeDef initStruct;
+	initStruct.GPIO_Mode = GPIO_Mode_OUT;
+	initStruct.GPIO_OType = GPIO_OType_PP;
+	initStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	initStruct.GPIO_Speed = GPIO_Speed_40MHz;
+	initStruct.GPIO_Pin = GPIO_Pin_5;
+	GPIO_Init(GPIOA,&initStruct);
+
+	return;
+
+}
+
 /**
 **===========================================================================
 **
